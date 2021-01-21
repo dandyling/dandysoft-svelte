bf5768637905f81fc52545e316fcc1d4ca6919bb
Dandy Ling <dandyling@gmail.com>
2021-01-22 04:09:58 +0800

    Add load more function for gallery

---
+++ src/Button.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  export let variant: "primary" | "secondary";
+  export let style: string = "";
+  export let onClick: () => void;
+</script>
+
+<button
+  on:click={onClick}
+  style="--bg-color: {variant === 'primary'
+    ? 'var(--brand-color)'
+    : 'white'};--color: {variant === 'primary'
+    ? 'white'
+    : 'var(--brand-color)'};{style}"><slot /></button
+>
+
+<style>
+  button {
+    background-color: var(--bg-color);
+    color: var(--color);
+    padding: 12px 30px;
+    border-radius: 0px;
+    font-size: 18px;
+    margin-bottom: 0px;
+    cursor: pointer;
+  }
+</style>
--- src/Gallery.svelte
+++ src/Gallery.svelte
@@ -1,20 +1,66 @@
 <script lang="ts">
   import Section from "./Section.svelte";
+  import Button from "./Button.svelte";
+  let showMore: boolean = false;
+
+  type Image = {
+    alt: string;
+    src: string;
+  };
+
+  let images: Image[] = [
+    {
+      alt: "Team 1",
+      src:
+        "https://static.wixstatic.com/media/469e00d2afd84d4998eb3742185505b1.jpg/v1/fill/w_1410,h_939,fp_0.50_0.50,q_90/469e00d2afd84d4998eb3742185505b1.webp",
+    },
+    {
+      alt: "Team 2",
+      src:
+        "https://static.wixstatic.com/media/4d2ad5a6300843d1bb5952efa6729423.jpg/v1/fill/w_1410,h_938,fp_0.50_0.50,q_90/4d2ad5a6300843d1bb5952efa6729423.webp",
+    },
+    {
+      alt: "Team 3",
+      src:
+        "https://static.wixstatic.com/media/1fb4c0ae2d604ff8b2f97e0a53bda09f.jpg/v1/fill/w_1410,h_935,fp_0.50_0.50,q_90/1fb4c0ae2d604ff8b2f97e0a53bda09f.webp",
+    },
+    {
+      alt: "Team 4",
+      src:
+        "https://static.wixstatic.com/media/d5838190cba0447aa103713c99db4229.jpg/v1/fill/w_777,h_518,fp_0.50_0.50,q_90/d5838190cba0447aa103713c99db4229.webp",
+    },
+    {
+      alt: "Team 5",
+      src:
+        "https://static.wixstatic.com/media/bafadadbe810eba6c2d754437f3164ca.jpg/v1/fill/w_779,h_518,fp_0.50_0.50,q_90/bafadadbe810eba6c2d754437f3164ca.webp",
+    },
+    {
+      alt: "Team 6",
+      src:
+        "https://static.wixstatic.com/media/9dc645b2ce0445109a23ddc9a4ad0efe.jpg/v1/fill/w_776,h_518,fp_0.50_0.50,q_90/9dc645b2ce0445109a23ddc9a4ad0efe.webp",
+    },
+  ];
+
+  const threshold = 3;
+
+  const onClick = () => {
+    showMore = true;
+  };
 </script>
 
 <Section bgColor="var(--brand-color)">
-  <img
-    alt="Team 1"
-    src="https://static.wixstatic.com/media/469e00d2afd84d4998eb3742185505b1.jpg/v1/fill/w_1410,h_939,fp_0.50_0.50,q_90/469e00d2afd84d4998eb3742185505b1.webp"
-  />
-  <img
-    alt="Team 2"
-    src="https://static.wixstatic.com/media/4d2ad5a6300843d1bb5952efa6729423.jpg/v1/fill/w_1410,h_938,fp_0.50_0.50,q_90/4d2ad5a6300843d1bb5952efa6729423.webp"
-  />
-  <img
-    alt="Team 3"
-    src="https://static.wixstatic.com/media/1fb4c0ae2d604ff8b2f97e0a53bda09f.jpg/v1/fill/w_1410,h_935,fp_0.50_0.50,q_90/1fb4c0ae2d604ff8b2f97e0a53bda09f.webp"
-  />
+  {#each images as image, i (image.src)}
+    {#if i < threshold}
+      <img alt={image.alt} src={image.src} />
+    {:else if showMore}
+      <img alt={image.alt} src={image.src} />
+    {/if}
+  {/each}
+  {#if !showMore}
+    <Button {onClick} style="margin-top:30px" variant="primary"
+      >Load More</Button
+    >
+  {/if}
 </Section>
 
 <style>
