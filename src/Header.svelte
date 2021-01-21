f2929152c5b3cf6d7f4b56de00550a84934bd555
Dandy Ling <dandyling@gmail.com>
2021-01-21 23:54:53 +0800

    Add header

---
+++ src/Header.svelte
@@ -0,0 +1,76 @@
+<script lang="ts">
+  type NavItem = {
+    name: string;
+    path: string;
+  };
+
+  let navItems: NavItem[] = [
+    {
+      name: "Home",
+      path: "/",
+    },
+    {
+      name: "Blog",
+      path: "/blog",
+    },
+    {
+      name: "CV",
+      path: "/cv",
+    },
+    {
+      name: "About",
+      path: "/about",
+    },
+    {
+      name: "Contact",
+      path: "/contact",
+    },
+    {
+      name: "Work",
+      path: "/work",
+    },
+    {
+      name: "Services",
+      path: "/services",
+    },
+  ];
+</script>
+
+<header>
+  <img alt="DandySoft logo" src="images/dandysoft-bw.png" />
+  <nav>
+    {#each navItems as n (n.path)}
+      <li>
+        <a href={n.path}>{n.name}</a>
+      </li>
+    {/each}
+  </nav>
+</header>
+
+<style>
+  header {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    height: 200px;
+    background-color: rgba(30, 32, 37);
+  }
+  img {
+    margin-top: 40px;
+    width: 70px;
+    height: 70px;
+  }
+  nav {
+    margin-top: 31px;
+    display: flex;
+    list-style: none;
+    justify-content: center;
+    width: 100%;
+  }
+  li {
+    padding: 0 10px;
+  }
+  a {
+    color: rgb(247, 247, 247);
+  }
+</style>
